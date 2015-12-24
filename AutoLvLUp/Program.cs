ef4596e15084ac6829cb4d464fbd19264b8a9465
dakotasblack <kotab@me.com>
2015-12-24 13:14:45 -0600

    Update Program.cs

--- AutoLvLUp/Program.cs
+++ AutoLvLUp/Program.cs
@@ -190,6 +190,9 @@ namespace AutoSpellUp
                 case "Heimerdinger":
                     AbilitySequence = new[] { 1, 2, 3, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
                     break;
+                case "Illaoi":
+                    AbilitySequence = new[] { 1, 2, 3, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
+                    break;
                 case "Irelia":
                     AbilitySequence = new[] { 3, 1, 2, 2, 2, 4, 2, 3, 2, 3, 4, 3, 3, 1, 1, 4, 1, 1 };
                     break;
