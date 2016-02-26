d75466897a33009644125193b83f0d43dffe94b1
dakotasblack <kotab@me.com>
2016-02-26 02:22:51 -0600

    Update Program.cs
    Updated kalsita level sequence

--- AutoLvLUp/Program.cs
+++ AutoLvLUp/Program.cs
@@ -247,7 +247,7 @@ namespace AutoSpellUp
                     AbilitySequence = new[] { 1, 3, 2, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3 };
                     break;
                 case "Kalista":
-                    AbilitySequence = new[] { 3, 1, 3, 2, 3, 4, 1, 3, 3, 1, 4, 1, 1, 2, 2, 4, 2, 2 };
+                    AbilitySequence = new[] { 3, 2, 3, 1, 3, 4, 2, 3, 3, 2, 4, 2, 2, 1, 1, 4, 1, 1 };
                     break;
                 case "Kayle":
                     AbilitySequence = new[] { 3, 1, 2, 3, 3, 4, 3, 1, 3, 1, 4, 1, 1, 2, 2, 4, 2, 2 };
